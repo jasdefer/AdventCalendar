89be1ad275bb92f1c379c3f35fec6488fd929ed4
Justus Bonz <justus.bonz@uni-hamburg.de>
2020-05-02 21:04:57 +0200

    Added note 04 and fixed access bugs

--- AdventCalendar/Source/AdventCalendarWebApp/Pages/2020/Igloo/TaskBoard/Note03.cshtml.cs
+++ AdventCalendar/Source/AdventCalendarWebApp/Pages/2020/Igloo/TaskBoard/Note03.cshtml.cs
@@ -79,7 +79,7 @@ namespace AdventCalendarWebApp.Pages._2020.Igloo.TaskBoard
 
         public IActionResult OnGet(string[] answer)
         {
-            if (!dayValidation.HasAccess(2))
+            if (!dayValidation.HasAccess(door))
             {
                 return RedirectToPage("Index", new { invalidDoor = door });
             }
---
+++ AdventCalendar/Source/AdventCalendarWebApp/Pages/2020/Igloo/TaskBoard/Note04.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using AdventCalendarWebApp.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdventCalendarWebApp.Pages._2020.Igloo.TaskBoard
+{
+    public class Note04Model : PageModel
+    {
+        private const int door = 4;
+        private readonly DayValidation dayValidation;
+
+        public Note04Model(DayValidation dayValidation)
+        {
+            this.dayValidation = dayValidation;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (!dayValidation.HasAccess(door))
+            {
+                return RedirectToPage("Index", new { invalidDoor = door });
+            }
+
+            return Page();
+        }
+    }
+}
