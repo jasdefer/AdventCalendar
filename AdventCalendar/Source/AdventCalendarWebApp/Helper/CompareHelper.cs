9d2374ca7eefc3299408435e376889133739e34d
Justus Bonz <justus.bonz@uni-hamburg.de>
2020-08-22 21:31:22 +0200

    Added note05

---
+++ AdventCalendar/Source/AdventCalendarWebApp/Helper/CompareHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventCalendarWebApp.Helper
+{
+    public static class CompareHelper
+    {
+        public static bool AreEqual(string s1, string s2)
+        {
+            return string.Equals(s1, s2, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public static bool Contains(IEnumerable<string> collection, string input)
+        {
+            if(collection == null)
+            {
+                return false;
+            }
+            foreach (var item in collection)
+            {
+                if(AreEqual(item, input))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
