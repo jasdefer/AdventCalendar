7e3164228714c0ded54198efcfa8edcce1d16de0
Justus Bonz <justus.bonz@uni-hamburg.de>
2020-10-27 18:02:06 +0100

    Added node 12

---
+++ AdventCalendar/Source/AdventCalendarWebApp/Pages/2020/Igloo/TaskBoard/Note12.cshtml.cs
@@ -0,0 +1,26 @@
+using AdventCalendarWebApp.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdventCalendarWebApp.Pages._2020.Igloo.TaskBoard
+{
+    public class Note12Model : PageModel
+    {
+        private readonly DayValidation dayValidation;
+        private const int door = 12;
+
+        public Note12Model(DayValidation dayValidation)
+        {
+            this.dayValidation = dayValidation;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (!dayValidation.HasAccess(door))
+            {
+                return RedirectToPage("Index", new { invalidDoor = door });
+            }
+            return Page();
+        }
+    }
+}
