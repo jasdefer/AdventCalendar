466c75340b0a1b2109d47bea8f22d4f57f948afa
Justus Bonz <justus.bonz@uni-hamburg.de>
2020-11-09 11:06:40 +0100

    Added page info

---
+++ AdventCalendar/Source/AdventCalendarWebApp/Pages/Misc/Info.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+
+namespace AdventCalendarWebApp.Pages.Misc
+{
+    public class InfoModel : PageModel
+    {
+        public string Version { get; set; }
+        public string PageDescription { get; set; }
+        public string[] ReleaseNotes { get; set; } = new string[0];
+
+        public void OnGet()
+        {
+            var assembly = typeof(Startup).Assembly;
+            Version = assembly.GetName().Version.ToString();
+            PageDescription = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+
+            //Read the release notes
+            //I did not find any way to read the release notes as I did for the above version and description
+            //So I just read the *.csproj file
+            var file = Path.Combine(Environment.CurrentDirectory, "AdventCalendarWebApp.csproj");
+            var xml = new XmlDocument();
+            xml.Load(file);
+            ReleaseNotes = xml.GetElementsByTagName("PackageReleaseNotes").Item(0).FirstChild.Value.Split('\n');
+        }
+    }
+}
