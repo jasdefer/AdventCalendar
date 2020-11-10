bb94d74317af44d8b5dca6aa2df65b45558cd306
Justus Bonz <justus.bonz@uni-hamburg.de>
2020-11-10 11:07:05 +0100

    Typo fixes

--- AdventCalendar/Source/AdventCalendarWebApp/Pages/Misc/Info.cshtml.cs
+++ AdventCalendar/Source/AdventCalendarWebApp/Pages/Misc/Info.cshtml.cs
@@ -8,37 +8,26 @@ namespace AdventCalendarWebApp.Pages.Misc
 {
     public class InfoModel : PageModel
     {
+        private static readonly string CsprojPath = Path.Combine(Environment.CurrentDirectory, "AdventCalendarWebApp.csproj");
         public string Version { get; set; }
         public string PageDescription { get; set; }
         public string[] ReleaseNotes { get; set; } = new string[0];
-        private static readonly string[] ProjectPaths = new string[]
-        {
-            Path.Combine(Environment.CurrentDirectory, "AdventCalendarWebApp.csproj"),
-            Path.Combine(Environment.SystemDirectory, "AdventCalendarWebApp.csproj"),
-            "AdventCalendarWebApp.csproj"
-        };
 
         public void OnGet()
         {
             var assembly = typeof(Startup).Assembly;
             Version = assembly.GetName().Version.ToString();
             PageDescription = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
-
+            
             //Read the release notes
             //I did not find any way to read the release notes as I did for the above version and description
             //So I just read the *.csproj file
-            var counter = 0;
-            while (!System.IO.File.Exists(ProjectPaths[counter]))
+            if (System.IO.File.Exists(CsprojPath))
             {
-                counter++;
-                if (counter >= ProjectPaths.Length)
-                {
-                    return;
-                }
+                var xml = new XmlDocument();
+                xml.Load(CsprojPath);
+                ReleaseNotes = xml.GetElementsByTagName("PackageReleaseNotes").Item(0).FirstChild.Value.Split('\n');
             }
-            var xml = new XmlDocument();
-            xml.Load(ProjectPaths[counter]);
-            ReleaseNotes = xml.GetElementsByTagName("PackageReleaseNotes").Item(0).FirstChild.Value.Split('\n');
         }
     }
 }
