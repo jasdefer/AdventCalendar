fcdc1b4faa4ad9697ce25e7c4ab1ad1c2777e099
Justus Bonz <justus.bonz@desior.net>
2020-12-19 18:06:26 +0100

    Start adding note22

---
+++ AdventCalendar/Source/AdventCalendarWebApp/Pages/2020/SnowmanForest.cshtml.cs
@@ -0,0 +1,39 @@
+using AdventCalendarWebApp.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdventCalendarWebApp.Pages._2020
+{
+    public class SnowmanForestModel : PageModel
+    {
+        private static readonly int[] sequence = new int[10] { 5, 2, 0, 9, 4, 8, 7, 6, 1, 3 };
+        private readonly DayValidation dayValidation;
+        private const int door = 22;
+
+        public bool CanSolve { get; set; }
+        public bool Solved { get; set; }
+        public int CurrentElfPosition { get; set; }
+
+        public SnowmanForestModel(DayValidation dayValidation)
+        {
+            this.dayValidation = dayValidation;
+        }
+
+        public IActionResult OnGet(int tree, int guess)
+        {
+            CanSolve = dayValidation.HasAccess(door);
+            guess++;
+            if (guess >= sequence.Length)
+            {
+                guess = 0;
+            }
+            ViewData["guess"] = guess;
+            CurrentElfPosition = sequence[guess];
+            if (tree == CurrentElfPosition)
+            {
+                Solved = true;
+            }
+            return Page();
+        }
+    }
+}
